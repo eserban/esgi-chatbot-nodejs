e029ee35261c160ef9392fe9e15f5cf884c42964
Eduard Serban <serbanedi.fr@gmail.com>
2021-03-22 14:20:13 +0100

    make it better

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.listen(PORT);
+
+app.get('/', (req, res) => {
+  res.send('Bonjour !');
+});
+
+app.get('/hello', (req, res) => {
+    if(req.query.nom){
+        res.send('Bonjour, ' + req.query.nom + " !");
+    }else{
+        res.send("Quel est votre nom ?");
+    }
+});
+
+app.post('/chat', function (req, res) {
+    if(req.body.msg === "ville"){
+        res.send("Nous sommes à Paris");
+    }else if(req.body.msg === "météo") {
+        res.send("Il fait beau");
+    }
+});
+
